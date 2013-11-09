6add639eee13e258ffea9b326cecbd84fad3f44d
Max Sperando <shadowmaxss@gmail.com>
2013-11-09 13:53:53 -0600

    update Wk 2.2

---
+++ Session1_Wk_2/blackjack_wk2_2.rb
@@ -0,0 +1,72 @@
+require 'rubygems'
+require 'pry'
+
+# Object Oriented Blackjack
+
+# 1) Abstraction
+# 2) Encapsulation
+
+class Card
+	attr_accessor :suit, :face_value
+
+	def initialize(s, fv)
+		@suit = s
+		@face_value = fv
+	end
+
+	def pretty_output
+		puts "The #{face_value} of #{find_suit}"	
+	end
+
+	def to_s
+		pretty_output
+	end
+
+	def find_suit
+		ret_val = case suit
+				when 'H' then 'Hearts'
+				when 'D' then 'Diamonds'
+				when 'S' then 'Spades'
+				when 'C' then 'Clubs'
+			end
+		ret_val
+	end
+end
+
+class Deck
+	attr_accessor :cards
+
+	def initialize
+		@cards = []
+		['H', 'D', 'S', 'C'].each do |suit|
+			['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+				@cards << Card.new(suit, face_value)
+			end
+		end
+		scramble!
+	end
+
+	def scramble!
+		cards.shuffle!
+	end
+
+	def deal_one
+	  cards.pop 
+	end
+
+	def size
+		cards.size
+	end
+end
+
+class Player
+end
+
+class Dealer
+end
+
+class Hand
+end
+
+deck = Deck.new
+binding.pry
